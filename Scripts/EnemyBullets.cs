f9af7cc0a06570527d0c1eaf7f8095a545d80936
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:18:42 +0200

    EnemyBullets.cs

--- Scripts/EnemyBullets.cs
+++ Scripts/EnemyBullets.cs
@@ -1,52 +1,54 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyBullets : MonoBehaviour {
-	float speed;    
-	Vector2 bulletDirection;    
-	bool isReady;    
+    float speed;
+    Vector2 bulletDirection;
+    bool isReady;
 
-	
-	void Awake() {
-		speed = 5f;
-		isReady = false;
-	}
+    // Metoda Awake se volá při inicializaci objektu
+    void Awake() {
+        speed = 5f;
+        isReady = false;
+    }
 
-	
-	void Start () {}
-	
-	
-	void Update () {
-		if ( isReady ) {
-			
-			Vector2 position = transform.position;
-		
-			position += bulletDirection * speed * Time.deltaTime;
-		
-			transform.position = position;
+    void Start () {}
 
-			
-			Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
-			
-			Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
-			if ( transform.position.x > max.x || transform.position.y > max.y || 
-				transform.position.x < min.x  || transform.position.y < min.y ) {
-				Destroy (gameObject);
-			}
-		}
-	}
+    void Update () {
+        // Pokud jsou střely připravené k pohybu
+        if (isReady) {
+            Vector2 position = transform.position;
 
-	
-	public void setBulletsDirection( Vector2 dir ) {
-		bulletDirection = dir.normalized;
-		isReady = true;
-	}
+            // Pohyb střely ve směru daném bulletDirection s danou rychlostí
+            position += bulletDirection * speed * Time.deltaTime;
 
-	void OnTriggerEnter2D(Collider2D _collider) {
-		
-		if ( _collider.tag == "PlayerShipTag" ) {
-			Destroy (gameObject);
-		}
-	}
+            // Aktualizace pozice střely
+            transform.position = position;
+
+            // Získání minimálního a maximálního bodu zobrazeného v okně kamery
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+            Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+            // Zničení střely, pokud překročí hranice okna kamery
+            if (transform.position.x > max.x || transform.position.y > max.y ||
+                transform.position.x < min.x || transform.position.y < min.y) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Metoda pro nastavení směru střely
+    public void setBulletsDirection(Vector2 dir) {
+        bulletDirection = dir.normalized;
+        isReady = true;
+    }
+
+    // Detekce kolize s objektem
+    void OnTriggerEnter2D(Collider2D _collider) {
+        // Zničení střely, pokud dojde ke kolizi s objektem označeným jako "PlayerShipTag"
+        if (_collider.tag == "PlayerShipTag") {
+            Destroy(gameObject);
+        }
+    }
 
 }
