1656db1caba318865187ecff1083859e80acfbf6
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:28:57 +0200

    TimeCounter.cs

--- Scripts/TimeCounter.cs
+++ Scripts/TimeCounter.cs
@@ -1,42 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class TimeCounter : MonoBehaviour {
-	Text timeUI;    // Time Counter UI
+    Text timeUI;    // UI prvek pro časový počítadlo
 
-	float startTime;    // 按下 play 時的時間
-	float elapsedTime;
-	bool startCounter;
+    float startTime;    // Čas po stisknutí tlačítka "play"
+    float elapsedTime;
+    bool startCounter;
 
-	float minutes;
-	float seconds;
-	// Use this for initialization
-	void Start () {
-		startCounter = false;
-		timeUI = GetComponent<Text> ();
-	}
+    float minutes;
+    float seconds;
 
-	// 開始時間倒數
-	public void startTimeCounter() {
-		startTime = Time.time;
-		startCounter = true;
-	}
+    void Start () {
+        startCounter = false;
+        timeUI = GetComponent<Text> ();    // Získání komponenty Text pro zobrazení času
+    }
 
-	// 停止倒數
-	public void stopTimeCounter() {
-		startCounter = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (startCounter) {
-			// 開始計算時間
-			elapsedTime = Time.time - startTime;
-			minutes = elapsedTime / 60;
-			seconds = elapsedTime % 60;
+    public void startTimeCounter() {
+        startTime = Time.time;    // Nastavení počátečního času na aktuální čas
+        startCounter = true;
+    }
 
-			timeUI.text = string.Format ("{0:00}:{1:00}", minutes, seconds);    // 更新介面
-		}
-	}
+    public void stopTimeCounter() {
+        startCounter = false;    // Zastavení čítání času
+    }
+
+    void Update () {
+        if (startCounter) {
+            // Počítání času
+            elapsedTime = Time.time - startTime;
+            minutes = elapsedTime / 60;
+            seconds = elapsedTime % 60;
+
+            timeUI.text = string.Format ("{0:00}:{1:00}", minutes, seconds);    // Aktualizace textu časového UI
+        }
+    }
 }
