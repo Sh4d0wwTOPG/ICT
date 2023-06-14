530fb72f33734688469d394b4dd7777938c53889
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:27:54 +0200

    Stars.cs

--- Scripts/Stars.cs
+++ Scripts/Stars.cs
@@ -1,23 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Stars : MonoBehaviour {
-	public float speed;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector2 position = transform.position;    // 取得目前位置
-		position = new Vector2(position.x, position.y + speed * Time.deltaTime);
-		transform.position = position;    // 更新位置
-		// 移動至畫面外時，將他重新移動到畫面上方
-		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2(0, 0));    // 底部左邊
-		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2(1, 1));    // 頂部右邊
-		if ( transform.position.y < min.y ) {
-			transform.position = new Vector2 (Random.Range (min.x, max.x), max.y);
-		}
-	}
+    public float speed;    // Rychlost pohybu hvězdy
+
+    void Start () {
+        // Prázdná metoda Start, není potřeba pro tento skript
+    }
+
+    void Update () {
+        Vector2 position = transform.position;    // Získání aktuální pozice hvězdy
+        position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+        transform.position = position;    // Aktualizace pozice hvězdy
+        // Pokud se hvězda přesune mimo obrazovku, přesuneme ji zpět na horní část obrazovky
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));    // Levý dolní roh
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));    // Pravý horní roh
+        if (transform.position.y < min.y) {
+            transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+        }
+    }
 }
