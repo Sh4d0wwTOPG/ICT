7ef8f71c262b85adcd78cd4418689d229c336a53
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:21:49 +0200

    EnemySpawner.cs

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -1,66 +1,65 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
-	public GameObject enemy;    
-	float maxSpawnRateInSecond = 5f;
+    public GameObject enemy;
+    float maxSpawnRateInSecond = 5f;
 
-	
-	void Start () {
-	}
-	
-	
-	void Update () {
-	}
+    void Start () {
+    }
 
-	
-	void SpawnEnemy() {
-		
-		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
-		
-		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
-		
-		GameObject enemy01 = (GameObject)Instantiate (enemy);
-		enemy01.transform.position = new Vector2 (Random.Range(min.x, max.x), max.y);
+    void Update () {
+    }
 
-		NextSpawnSheduler ();
-	}
+    // Metoda pro vytvoření nepřátelského objektu
+    void SpawnEnemy() {
+        // Získání minimálního a maximálního bodu zobrazeného v okně kamery
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-	
-	void NextSpawnSheduler() {
-		float spawnInNSec;
-		if ( maxSpawnRateInSecond > 1f ) {
-			
-			spawnInNSec = Random.Range (1f, maxSpawnRateInSecond);
-		} else {
-			spawnInNSec = 1f;
-		}
-		Invoke ("SpawnEnemy", spawnInNSec);
-	}
+        // Vytvoření nepřátelského objektu a umístění na náhodnou pozici v horní části obrazovky
+        GameObject enemy01 = (GameObject)Instantiate(enemy);
+        enemy01.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
 
-	
-	void IncreseSpawnRate() {
-		if ( maxSpawnRateInSecond > 1f ) {
-			maxSpawnRateInSecond--;
-		}
+        // Naplánování dalšího vytvoření nepřátelského objektu
+        NextSpawnScheduler();
+    }
 
-		if ( maxSpawnRateInSecond == 1f ) {
-			CancelInvoke ("IncreseSpawnRate");
-		}
-	}
+    // Metoda pro naplánování dalšího vytvoření nepřátelského objektu
+    void NextSpawnScheduler() {
+        float spawnInNSec;
+        if (maxSpawnRateInSecond > 1f) {
+            // Náhodný časový interval pro vytvoření dalšího nepřátelského objektu
+            spawnInNSec = Random.Range(1f, maxSpawnRateInSecond);
+        } else {
+            spawnInNSec = 1f;
+        }
+        Invoke("SpawnEnemy", spawnInNSec);
+    }
 
-	
-	public void StartEnemySpawn() {
-		maxSpawnRateInSecond = 5f;    
-		Invoke ("SpawnEnemy", maxSpawnRateInSecond);
-		
-		InvokeRepeating("IncreseSpawnRate", 0f, 30f);
-	}
+    // Metoda pro zvýšení rychlosti vytváření nepřátelských objektů
+    void IncreaseSpawnRate() {
+        if (maxSpawnRateInSecond > 1f) {
+            maxSpawnRateInSecond--;
+        }
 
-	
-	public void StopEnemySpawn() {
-		CancelInvoke ("SpawnEnemy");
-		CancelInvoke ("IncreseSpawnRate");
-	}
+        if (maxSpawnRateInSecond == 1f) {
+            CancelInvoke("IncreaseSpawnRate");
+        }
+    }
 
+    // Metoda pro spuštění vytváření nepřátelských objektů
+    public void StartEnemySpawn() {
+        maxSpawnRateInSecond = 5f;
+        Invoke("SpawnEnemy", maxSpawnRateInSecond);
+
+        // Pravidelné zvyšování rychlosti vytváření nepřátelských objektů každých 30 sekund
+        InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
+    }
+
+    // Metoda pro zastavení vytváření nepřátelských objektů
+    public void StopEnemySpawn() {
+        CancelInvoke("SpawnEnemy");
+        CancelInvoke("IncreaseSpawnRate");
+    }
 }
