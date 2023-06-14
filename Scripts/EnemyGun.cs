f7efef68356549bc3264b4f35d78a8f9ba3a4053
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:20:34 +0200

    EnemyGun.cs

--- Scripts/EnemyGun.cs
+++ Scripts/EnemyGun.cs
@@ -1,46 +1,43 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyGun : MonoBehaviour {
-	public GameObject enemyBullets;    
-	public GameObject enemyObject;    
-	
-	void Start () {
-		
-		Invoke ("EnemyFireBullets", 1f);
-	}
-	
-	
-	void Update () {
-	}
+    public GameObject enemyBullets;
+    public GameObject enemyObject;
 
-	
-	void EnemyFireBullets() {
-		
-		GameObject playerAirplane = GameObject.Find ("AirCraft");
-		if (playerAirplane != null) {
-			enemyObject.GetComponent<EnemyController> ().animationChooser ("shooting");
+    void Start () {
+        // Zavolání metody EnemyFireBullets s časovým zpožděním 1 sekundy
+        Invoke ("EnemyFireBullets", 1f);
+    }
 
-			
-			GameObject bullets = (GameObject)Instantiate (enemyBullets);
-			
-			GameObject bullets2 = (GameObject)Instantiate (enemyBullets);
-			
-			GameObject bullets3 = (GameObject)Instantiate (enemyBullets);
+    void Update () {
+    }
 
-		
-			bullets.transform.position = transform.position;
-			bullets2.transform.position = transform.position;
-			bullets3.transform.position = transform.position;
+    // Metoda pro vystřelení střel nepřátelského objektu
+    void EnemyFireBullets() {
+        // Hledání objektu hráčova letadla
+        GameObject playerAirplane = GameObject.Find ("AirCraft");
+        if (playerAirplane != null) {
+            // Spuštění animace střelby u nepřátelského objektu
+            enemyObject.GetComponent<EnemyController> ().animationChooser ("shooting");
 
-			
-			Vector2 direction = playerAirplane.transform.position - bullets.transform.position;
+            // Vytvoření tří nepřátelských střel
+            GameObject bullets = (GameObject)Instantiate (enemyBullets);
+            GameObject bullets2 = (GameObject)Instantiate (enemyBullets);
+            GameObject bullets3 = (GameObject)Instantiate (enemyBullets);
 
-			
-			bullets.GetComponent<EnemyBullets> ().setBulletsDirection (direction);
-			bullets2.GetComponent<EnemyBullets> ().setBulletsDirection (new Vector2(direction.x + 2, direction.y));
-			bullets3.GetComponent<EnemyBullets> ().setBulletsDirection (new Vector2(direction.x - 2, direction.y));
-		}
-	}
+            // Nastavení pozice střel na pozici nepřátelského objektu
+            bullets.transform.position = transform.position;
+            bullets2.transform.position = transform.position;
+            bullets3.transform.position = transform.position;
 
+            // Výpočet směru střelby na základě pozice hráčova letadla
+            Vector2 direction = playerAirplane.transform.position - bullets.transform.position;
+
+            // Nastavení směru střelby pro jednotlivé střely
+            bullets.GetComponent<EnemyBullets> ().setBulletsDirection (direction);
+            bullets2.GetComponent<EnemyBullets> ().setBulletsDirection (new Vector2(direction.x + 2, direction.y));
+            bullets3.GetComponent<EnemyBullets> ().setBulletsDirection (new Vector2(direction.x - 2, direction.y));
+        }
+    }
 }
