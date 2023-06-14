0dd64a4db906c18906dee71e55c6ed0a8867a431
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:22:40 +0200

    GameManager.cs

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,76 +1,83 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	
-	public GameObject playButton;
-	public GameObject airplane;
-	public GameObject enemySpawner;
-	public GameObject gameOverSprite;    
-	public GameObject userScore;   
-	public GameObject gameTimer;    
-	public GameObject gameTitle;   
+    public GameObject playButton;
+    public GameObject airplane;
+    public GameObject enemySpawner;
+    public GameObject gameOverSprite;
+    public GameObject userScore;
+    public GameObject gameTimer;
+    public GameObject gameTitle;
 
-	public enum GameStates {
-		Opening, GamePlay, GameOver
-	}
+    // Výčet stavů hry
+    public enum GameStates {
+        Opening, GamePlay, GameOver
+    }
 
-	GameStates gameStates;
-	
-	void Start () {
-	}
-	
-	
-	void UpdateGameStates () {
-		switch(gameStates) {
-		case GameStates.GameOver:
-			
-			gameTimer.GetComponent<TimeCounter>().stopTimeCounter();
-			
-			enemySpawner.GetComponent<EnemySpawner>().StopEnemySpawn();
-			
-			gameOverSprite.SetActive(true);
-			
-			Invoke("SetGameStateToOpening", 8f);
-			break;
-		case GameStates.GamePlay:
-			
-			userScore.GetComponent<GameScore> ().Score = 0;
-			
-			playButton.SetActive (false);
-			gameTitle.SetActive (false);
-			
-			airplane.GetComponent<UserControll>().Init();
-			
-			enemySpawner.GetComponent<EnemySpawner>().StartEnemySpawn();
-			
-			gameTimer.GetComponent<TimeCounter>().startTimeCounter();
+    GameStates gameStates;
 
-			break;
-		case GameStates.Opening:
-			
-			gameOverSprite.SetActive (false);
-			
-			playButton.SetActive (true);
-			gameTitle.SetActive (true);
-			break;
-		}
-	}
+    void Start () {
+    }
 
-	
-	public void SetGameState(GameStates state) {
-		gameStates = state;
-		UpdateGameStates ();
-	}
+    // Metoda pro aktualizaci stavů hry
+    void UpdateGameStates () {
+        switch(gameStates) {
+            case GameStates.GameOver:
+                // Zastavení časovače hry
+                gameTimer.GetComponent<TimeCounter>().stopTimeCounter();
 
-	
-	public void StartGamePlay() {
-		gameStates = GameStates.GamePlay;
-		UpdateGameStates ();
-	}
+                // Zastavení vytváření nepřátelských objektů
+                enemySpawner.GetComponent<EnemySpawner>().StopEnemySpawn();
 
-	
-	public void SetGameStateToOpening() {
-		SetGameState (GameStates.Opening);
-	}
+                // Zobrazení obrazovky s nápisem "Game Over"
+                gameOverSprite.SetActive(true);
+
+                // Po 8 sekundách nastavení stavu hry na "Opening"
+                Invoke("SetGameStateToOpening", 8f);
+                break;
+            case GameStates.GamePlay:
+                // Resetování skóre hráče na 0
+                userScore.GetComponent<GameScore>().Score = 0;
+
+                // Skrytí tlačítka Play a názvu hry
+                playButton.SetActive(false);
+                gameTitle.SetActive(false);
+
+                // Inicializace ovládání hráčova letadla
+                airplane.GetComponent<UserControll>().Init();
+
+                // Spuštění vytváření nepřátelských objektů
+                enemySpawner.GetComponent<EnemySpawner>().StartEnemySpawn();
+
+                // Spuštění časovače hry
+                gameTimer.GetComponent<TimeCounter>().startTimeCounter();
+                break;
+            case GameStates.Opening:
+                // Skrytí obrazovky s nápisem "Game Over"
+                gameOverSprite.SetActive(false);
+
+                // Zobrazení tlačítka Play a názvu hry
+                playButton.SetActive(true);
+                gameTitle.SetActive(true);
+                break;
+        }
+    }
+
+    // Metoda pro nastavení stavu hry
+    public void SetGameState(GameStates state) {
+        gameStates = state;
+        UpdateGameStates();
+    }
+
+    // Metoda pro spuštění hry
+    public void StartGamePlay() {
+        gameStates = GameStates.GamePlay;
+        UpdateGameStates();
+    }
+
+    // Metoda pro nastavení stavu hry na "Opening"
+    public void SetGameStateToOpening() {
+        SetGameState(GameStates.Opening);
+    }
 }
