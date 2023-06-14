30a52d59b3039cd5192de98ac5db39703e6ffaae
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:16:45 +0200

    BulletsControll.cs

--- Scripts/BulletsControll.cs
+++ Scripts/BulletsControll.cs
@@ -1,38 +1,39 @@
-﻿using UnityEngine;
+Copy code
+using UnityEngine;
 using System.Collections;
 
 public class BulletsControll : MonoBehaviour {
-	float speed;
-	
-	void Start () {
-		speed = 8f;
-	}
-	
-	
-	void Update () {
-		
-		Vector2 position = transform.position;
+    float speed;
+    
+    void Start () {
+        speed = 8f;
+    }
+    
+    
+    void Update () {
+        
+        Vector2 position = transform.position;
 
-		
-		position = new Vector2 (position.x, position.y + speed * Time.deltaTime);
+        // Pohyb bulletu směrem nahoru se zadanou rychlostí
+        position = new Vector2 (position.x, position.y + speed * Time.deltaTime);
 
-		
-		transform.position = position;
-		
+        // Aktualizace pozice bulletu
+        transform.position = position;
+        
+        // Získání maximálních hodnoty X a Y zobrazených v okně kamery
+        Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+        
+        // Zničení bulletu, pokud překročí horní okraj kamerového pohledu
+        if (transform.position.y > max.y) {
+            Destroy (gameObject);
+        }
+    }
 
-		
-		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
-
-		
-		if (transform.position.y > max.y) {
-			Destroy (gameObject);
-		}
-	}
-
-	void OnTriggerEnter2D(Collider2D _collider) {
-		
-		if ( _collider.tag == "EnemyShipTag" ) {
-			Destroy (gameObject);
-		}
-	}
+    // Detekce kolize s objektem
+    void OnTriggerEnter2D(Collider2D _collider) {
+        // Zničení bulletu, pokud dojde ke kolizi s objektem označeným jako "EnemyShipTag"
+        if (_collider.tag == "EnemyShipTag") {
+            Destroy (gameObject);
+        }
+    }
 }
