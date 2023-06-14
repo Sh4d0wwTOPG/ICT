8d8b48fa5759a8ebd16d4c736e0330c421bb69fb
Sh4d0wwTOPG <119569895+Sh4d0wwTOPG@users.noreply.github.com>
2023-06-14 08:19:43 +0200

    EnemyController.cs

--- Scripts/EnemyController.cs
+++ Scripts/EnemyController.cs
@@ -1,60 +1,65 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyController : MonoBehaviour {
-	float speed;
-	public int score;
-	public int life;
-	public GameObject exposion;    
-	GameObject scoreTextUI;    
-	private Animator anim;
-	
-	void Start () {
-		speed = 2f;    
-		scoreTextUI = GameObject.FindGameObjectWithTag("ScoreUITag");
-		anim = GetComponent<Animator> ();
+    float speed;
+    public int score;
+    public int life;
+    public GameObject explosion;
+    GameObject scoreTextUI;
+    private Animator anim;
 
-	}
-	
-	
-	void Update () {
-		
-		Vector2 position = transform.position;
+    void Start () {
+        speed = 2f;
+        scoreTextUI = GameObject.FindGameObjectWithTag("ScoreUITag");
+        anim = GetComponent<Animator>();
+    }
 
-		
-		position = new Vector2 (position.x, position.y - speed * Time.deltaTime);
-		transform.position = position;
+    void Update () {
+        Vector2 position = transform.position;
 
-		
-		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-		if ( transform.position.y < min.y ) {
-			Destroy (gameObject);
-		}
-	}
+        // Pohyb nepřátelského objektu dolů se zadanou rychlostí
+        position = new Vector2 (position.x, position.y - speed * Time.deltaTime);
+        transform.position = position;
 
-	void OnTriggerEnter2D(Collider2D _collider) {
-		
-		if ( _collider.tag == "PlayerBulletTag" ) {
-			life--;
-			if ( life == 0 ) {
-				Destroy (gameObject);
-				scoreTextUI.GetComponent<GameScore> ().Score += score;    
-				GameObject expo = (GameObject)Instantiate (exposion);
-				expo.transform.position = transform.position;
-			}
-		}
-	}
+        // Získání minimálního bodu zobrazeného v okně kamery
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
 
-	public void animationChooser(string method) {
-		switch(method) {
-		case "shooting":
-			anim.SetBool ("shooting", true);
-			break;
-		case "unshooting":
-			anim.SetBool ("shooting", false);
-			break;
-		default:
-			break;
-		}
-	}
+        // Zničení nepřátelského objektu, pokud překročí dolní okraj kamerového pohledu
+        if (transform.position.y < min.y) {
+            Destroy (gameObject);
+        }
+    }
+
+    // Detekce kolize s objektem
+    void OnTriggerEnter2D(Collider2D _collider) {
+        // Pokud dojde ke kolizi s objektem označeným jako "PlayerBulletTag"
+        if (_collider.tag == "PlayerBulletTag") {
+            life--;
+
+            // Pokud dojde k životům nepřátelského objektu, zničí se a přičte se skóre
+            if (life == 0) {
+                Destroy (gameObject);
+                scoreTextUI.GetComponent<GameScore> ().Score += score;
+
+                // Vytvoření a umístění exploze na pozici zničeného objektu
+                GameObject expo = (GameObject)Instantiate (explosion);
+                expo.transform.position = transform.position;
+            }
+        }
+    }
+
+    // Metoda pro výběr animace
+    public void animationChooser(string method) {
+        switch(method) {
+            case "shooting":
+                anim.SetBool ("shooting", true);
+                break;
+            case "unshooting":
+                anim.SetBool ("shooting", false);
+                break;
+            default:
+                break;
+        }
+    }
 }
